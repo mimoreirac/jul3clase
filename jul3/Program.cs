e3954534951195047f47524f99b413a6659e1e38
Estudiante Ltic <estudiante@sistemas.local>
2024-07-03 14:44:39 -0500

    Añade factorial

---
+++ jul3/Program.cs
@@ -0,0 +1,12 @@
+﻿// Factorial de un Numero
+Console.WriteLine("~~~~~~~~~~~~ Factorial de un número ~~~~~~~~~~~~~");
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+int resultado = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    resultado = resultado*i;
+}
+
+Console.WriteLine(resultado);
