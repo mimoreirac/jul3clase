b22f4e7a7c73d427dfd47cdbc5c453835648103e
Estudiante Ltic <estudiante@sistemas.local>
2024-07-03 15:21:06 -0500

    Añade mayor menor y promedio

---
+++ mayormenor/Program.cs
@@ -0,0 +1,26 @@
+﻿// Número mayor, menor y promedio
+Console.WriteLine("~~~~Mayor, Menor, Promedio~~~~");
+Console.WriteLine("Cuántos números va a ingresar=?");
+int cant_num = int.Parse(Console.ReadLine());
+int mayor,menor,acumulador;
+mayor = menor = acumulador = 0;
+
+for (int i = 1; i <= cant_num; i++)
+{
+    Console.WriteLine("Ingrese un número");
+    int num = int.Parse(Console.ReadLine());
+    if (i == 1 | num > mayor)
+    {
+        mayor = num;
+    }
+    if (i == 1 | num < menor)
+    {
+        menor = num;
+    }
+    acumulador += num;
+}
+int resultado = acumulador/cant_num;
+Console.WriteLine($"El número mayor es {mayor}.");
+Console.WriteLine($"El número menor es {menor}.");
+Console.WriteLine($"El promedio es {resultado}.");
+
